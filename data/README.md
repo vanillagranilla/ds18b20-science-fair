fe1d84bb94730848d5e964e7e8d8d1248e7c157f
Jim Gronowski <jim.gronowski@gmail.com>
2022-12-22 19:19:11 +0000

    working prototype

---
+++ data/README.md
@@ -0,0 +1,1 @@
+Output files will go here.   I'm calling this with cron, so each run gets it's own CSV file.   I'll concatenate them together at the end of the experiment.
--- getTemps.go
+++ getTemps.go
@@ -1,23 +1,53 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
-
 	"github.com/yryz/ds18b20"
+	"os"
+	"strconv"
+	"time"
 )
 
 func main() {
-	sensors, err := ds18b20.Sensors()
-	if err != nil {
-		panic(err)
-	}
+	loc, e := time.LoadLocation("America/Los_Angeles")
+	CheckError(e)
 
-	fmt.Printf("sensor IDs: %v\n", sensors)
+	myTime := time.Now().In(loc)
+	timestamp := myTime.Format(time.RFC3339)
+	const numSensors int = 2 //hardcoding this - a bit of a hack
+
+	sensors, e := ds18b20.Sensors()
+	CheckError(e)
+
+	//fmt.Printf("sensor IDs: %v\n", sensors)
+
+	var data [][]string
 
 	for _, sensor := range sensors {
-		t, err := ds18b20.Temperature(sensor)
+		temp, err := ds18b20.Temperature(sensor)
 		if err == nil {
-			fmt.Printf("sensor: %s temperature: %.2f°C\n", sensor, t)
+			//		fmt.Printf("sensor: %s temperature: %.2f°C\n", sensor, temp)
+			e := []string{timestamp, sensor, strconv.FormatFloat(temp, 'f', 3, 32)}
+
+			//		fmt.Printf("e: %v\n", e)
+			data = append(data, e)
 		}
+	}
+
+	//fmt.Printf("data: %v\n", data)
+
+	file, e := os.Create("./data/" + timestamp + ".csv")
+	CheckError(e)
+
+	writer := csv.NewWriter(file)
+
+	writer.WriteAll(data)
+}
+
+func CheckError(e error) {
+	if e != nil {
+		fmt.Println(e)
+		panic(e)
 	}
 }
